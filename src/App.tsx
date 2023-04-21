aa12cf4a69a188a30ce761e572d9cc0b62fbc5f8
Maxi Ferreira <charca@gmail.com>
2023-04-21 04:51:21 +0000

    Basic App Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import HelpScout from "@helpscout/javascript-sdk";
+import { Button, DefaultStyle, Heading } from "@helpscout/ui-kit";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [userEmail, setUserEmail] = useState<string | undefined>(
+    "unknown user"
+  );
+
+  useEffect(() => {
+    HelpScout.getApplicationContext().then(({ user }) =>
+      setUserEmail(user?.email)
+    );
+  }, []);
+
+  function onClick() {
+    HelpScout.showNoty("success", "Hello from the sidebar app");
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite!</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
-
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DefaultStyle />
+      <Heading level="h4">Hi, {userEmail}</Heading>
+      <br />
+      <Button size="sm" onClick={onClick}>
+        Click me
+      </Button>
     </div>
   );
 }
