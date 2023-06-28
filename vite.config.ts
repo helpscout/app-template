c1ab986ae7191675952dd4f09277c261d3b26273
Deiva Magalhaes <deivamagalhaes@helpscout.com>
2023-06-28 16:20:07 -0700

    Tweak Codesandbox config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import mkcert from 'vite-plugin-mkcert'
+import mkcert from "vite-plugin-mkcert";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
